640a85867b1af6de062baa3591d2d1132c661ed7
Henrique Mandt <henrique.mandt@outlook.com>
2020-10-12 16:25:06 -0300

    Inserted products in INSTALLED_APPS and register it in admin.py

--- products/models.py
+++ products/models.py
@@ -5,4 +5,4 @@ from django.db import models
 class Product(models.Model):
     title       = models.CharField(max_length=120)
     description = models.TextField()
-    price       = models.DecimalField(decimal_places=2, max_digitis=20, defacult=100.00)
+    price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
