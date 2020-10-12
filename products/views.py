a1ada45f9d4526fc4a4a503a403110508bf1e53e
Henrique Mandt <henrique.mandt@outlook.com>
2020-10-12 16:45:16 -0300

    Created CBV and FBV in views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from .models import Product
+
+class ProductListView(ListView):
+    queryset    = Product.objects.all()
+
+#function based view
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'qs': queryset
+    }
+    return render(request, "products/list.html", context)
