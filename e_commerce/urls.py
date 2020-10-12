79460d4b829ee3c740778b92fbe1aeb5b688b674
Henrique Mandt <henrique.mandt@outlook.com>
2020-10-12 16:49:09 -0300

    Imported CBV and FBV in urls.py

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_page, about_page, contact_page, login_page, register_page
 
 from django.conf import settings
 from django.conf.urls.static import static
+
+from products.views import ProductListView, product_list_view
+from .views import home_page, about_page, contact_page, login_page, register_page
+
 
 urlpatterns = [
     path('', home_page),
@@ -26,7 +29,10 @@ urlpatterns = [
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('products/', ProductListView.as_view())
+    path('products-fbv/', product_list_view)
     path('admin/', admin.site.urls),
+
 ]
 
 if settings.DEBUG:
